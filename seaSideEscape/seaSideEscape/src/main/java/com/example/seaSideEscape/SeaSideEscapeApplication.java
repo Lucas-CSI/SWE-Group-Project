6e49868504b0c2a9688700afa675f5101fb9c0d0
Lucas-CSI <151797060+Lucas-CSI@users.noreply.github.com>
2024-10-09 12:56:22 -0500

    initial springboot

---
+++ seaSideEscape/seaSideEscape/src/main/java/com/example/seaSideEscape/SeaSideEscapeApplication.java
@@ -0,0 +1,13 @@
+package com.example.seaSideEscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaSideEscapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaSideEscapeApplication.class, args);
+	}
+
+}
---
+++ seaSideEscape/seaSideEscape/src/test/java/com/example/seaSideEscape/SeaSideEscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seaSideEscape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaSideEscapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
