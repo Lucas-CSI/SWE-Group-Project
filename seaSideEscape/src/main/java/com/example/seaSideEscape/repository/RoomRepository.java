f627d0b60e640d8987c4baa44cd862fa3a8051d4
Lucas-CSI <151797060+Lucas-CSI@users.noreply.github.com>
2024-10-27 21:20:52 -0500

    fixed query for bookRoom usage

--- seaSideEscape/src/main/java/com/example/seaSideEscape/repository/RoomRepository.java
+++ seaSideEscape/src/main/java/com/example/seaSideEscape/repository/RoomRepository.java
@@ -13,6 +13,6 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
     @Query("SELECT u FROM Room u WHERE u.theme = ?1")
     Optional<Room> findByTheme(String theme);
 
-    @Query("SELECT u FROM Room u WHERE u.oceanView = ?1 AND u.theme = ?2 AND u.isBooked = ?3")
-    Optional<Room> findByViewByThemeAndBooked(boolean oceanView, Room.Themes theme, boolean booked);
+    @Query("SELECT u FROM Room u WHERE u.qualityLevel = ?1 AND u.bedType = ?2 AND u.oceanView = ?3 AND u.theme = ?4 AND u.isBooked = ?5")
+    Optional<Room> findByQualityLevelAndBedTypeAndViewAndThemeAndBooked(String qualityLevel, boolean oceanView, Room.Themes theme, boolean booked);
 }
