6ece9c061c6160405ab4c05ae84c8b17cf208795
Lucas-CSI <151797060+Lucas-CSI@users.noreply.github.com>
2024-10-27 22:20:24 -0500

    readded PaymentService

---
+++ seaSideEscape/src/main/java/com/example/seaSideEscape/service/PaymentService.java
@@ -0,0 +1,41 @@
+package com.example.seaSideEscape.Service;
+
+import com.example.seaSideEscape.model.Payment;
+import com.example.seaSideEscape.model.Reservation;
+import com.example.seaSideEscape.repository.PaymentRepository;
+import com.example.seaSideEscape.repository.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Service
+public class PaymentService {
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    private  ReservationRepository reservationRepository;
+
+    public Payment processPayment(Long reservationId, String paymentMethod, String billingAddress, BigDecimal amount){
+
+        Reservation reservation = reservationRepository.findById(reservationId)
+                .orElseThrow(() -> new IllegalArgumentException("Reservation not found"));
+        
+        Payment payment = new Payment();
+        payment.setReservation(reservation);
+        payment.setPaymentMethod(paymentMethod);
+        payment.setBillingAddress(billingAddress);
+        payment.setAmount(amount);
+        payment.setPaymentDate(LocalDateTime.now());
+        payment.setSuccess(true); //Assume for now
+
+        paymentRepository.save(payment);
+
+        reservation.setPaid(true);
+        reservationRepository.save(reservation);
+        return payment;
+    }
+}
