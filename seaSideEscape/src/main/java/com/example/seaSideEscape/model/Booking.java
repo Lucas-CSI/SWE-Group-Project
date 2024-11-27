72380a36775e7041a500d2d4ef419486e7150a12
Lucas-CSI <151797060+Lucas-CSI@users.noreply.github.com>
2024-11-26 20:24:17 -0600

    added booking entity

---
+++ seaSideEscape/src/main/java/com/example/seaSideEscape/model/Booking.java
@@ -0,0 +1,40 @@
+package com.example.seaSideEscape.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Reservation reservation;
+    @ManyToOne
+    private Room room;
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
