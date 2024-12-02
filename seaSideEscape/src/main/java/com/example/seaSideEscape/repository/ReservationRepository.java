bd404cf7b0d4151a5832884bf753561c68bfe2e1
Lucas-CSI <151797060+Lucas-CSI@users.noreply.github.com>
2024-12-01 18:01:19 -0600

    commented bugged query

--- seaSideEscape/src/main/java/com/example/seaSideEscape/repository/ReservationRepository.java
+++ seaSideEscape/src/main/java/com/example/seaSideEscape/repository/ReservationRepository.java
@@ -13,9 +13,9 @@ import java.util.Optional;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    @Query("SELECT u FROM Reservation u JOIN u.rooms rm WHERE " +
-            "rm.isSmokingAllowed = ?1 AND rm.qualityLevel = ?2 AND rm.bedType = ?3 AND " +
-            "rm.oceanView = ?4 AND rm.theme = ?5 AND u.endDate >= ?6 AND u.startDate <= ?7")
-    List<Reservation> findBySmokingAllowedByQualityLevelAndBedTypeAndViewAndThemeBetweenCheckInDateAndCheckOutDate(boolean isSmokingAllowed, Room.QualityLevel qualityLevel, String bedType, boolean oceanView, Room.Themes theme, LocalDate startDate, LocalDate endDate);
+   // @Query("SELECT u FROM Reservation u JOIN u.rooms rm WHERE " +
+        //    "rm.isSmokingAllowed = ?1 AND rm.qualityLevel = ?2 AND rm.bedType = ?3 AND " +
+      //      "rm.oceanView = ?4 AND rm.theme = ?5 AND u.endDate >= ?6 AND u.startDate <= ?7")
+    //List<Reservation> findBySmokingAllowedByQualityLevelAndBedTypeAndViewAndThemeBetweenCheckInDateAndCheckOutDate(boolean isSmokingAllowed, Room.QualityLevel qualityLevel, String bedType, boolean oceanView, Room.Themes theme, LocalDate startDate, LocalDate endDate);
 }
 
