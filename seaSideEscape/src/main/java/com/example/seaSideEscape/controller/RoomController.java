8792e90235bcc966935a69bbda43e5c119606fa8
Lucas-CSI <151797060+Lucas-CSI@users.noreply.github.com>
2024-12-04 12:17:51 -0600

    implemented serializemodule

--- seaSideEscape/src/main/java/com/example/seaSideEscape/controller/RoomController.java
+++ seaSideEscape/src/main/java/com/example/seaSideEscape/controller/RoomController.java
@@ -1,7 +1,9 @@
 package com.example.seaSideEscape.controller;
 
+import com.example.seaSideEscape.SerializeModule;
 import com.example.seaSideEscape.model.Room;
 import com.example.seaSideEscape.service.RoomService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,20 +11,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @RestController
 public class RoomController {
-    @Autowired
     private final RoomService roomService;
+    private final SerializeModule<Room> serializeModule = new SerializeModule<>();
 
+    @Autowired
     public RoomController(RoomService roomService) {
         this.roomService = roomService;
     }
 
     @PostMapping("/getAvailableRooms")
-    public List<Room> getAvailableRooms(@RequestParam("checkInDate")
-                                        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate checkInDate, @RequestParam("checkOutDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate checkOutDate) {
-        return roomService.getAvailableRooms(checkInDate, checkOutDate);
+    public String getAvailableRooms(@RequestParam("checkInDate")
+                                        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate checkInDate, @RequestParam("checkOutDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate checkOutDate) throws JsonProcessingException {
+        return serializeModule.listToJSON(roomService.getAvailableRooms(checkInDate, checkOutDate));
     }
 }
