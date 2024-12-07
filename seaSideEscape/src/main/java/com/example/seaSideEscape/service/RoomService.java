417e653758802d20493cce5d18b744afa369133a
Lucas-CSI <151797060+Lucas-CSI@users.noreply.github.com>
2024-12-07 16:07:22 -0600

    Updated room generation

--- seaSideEscape/src/main/java/com/example/seaSideEscape/service/RoomService.java
+++ seaSideEscape/src/main/java/com/example/seaSideEscape/service/RoomService.java
@@ -38,20 +38,27 @@ public class RoomService {
     }
 
     public Room findSpecificAvailableRoom(Room room, LocalDate checkInDate, LocalDate checkOutDate){
-        return roomRepository.findSpecificAvailableRooms(room.isSmokingAllowed(),room.getQualityLevel(), room.getBedType(), room.isOceanView(), room.getTheme(), checkInDate, checkOutDate).getFirst();
+        List<Room> rooms = roomRepository.findSpecificAvailableRooms(room.isSmokingAllowed(),room.getQualityLevel(), room.getBedType(), room.isOceanView(), room.getTheme(), checkInDate, checkOutDate);
+        if(rooms == null || rooms.isEmpty()) return null;
+        return rooms.getFirst();
     }
 
     static String[] bedTypes = {"Queen", "King"};
+
     public void setupDB(){
         for(int i = 0; i < 100; ++i){
             Room room = new Room();
+            room.setQualityLevel(Room.QualityLevel.values()[(int)(Math.random() * 4)]);
             room.setSmokingAllowed((int) (Math.random() * 5) == 0);
-            room.setBedType(bedTypes[(int) (Math.random() * 2)]);
-            room.setRoomNumber(String.valueOf(i));
-            room.setId((long) (-i));
+            room.setQualityLevel(Room.QualityLevel.values()[(int)(Math.random() * 4)]);
+            if(room.getQualityLevel().ordinal() <= 1){
+                room.setBedType("Queen");
+            }else{
+                room.setBedType("King");
+            }
+            room.setRoomNumber(String.valueOf(i + 101));
             room.setTheme(Room.Themes.values()[(int) (Math.random() * 3)]);
             room.setOceanView((int)(Math.random() * 2) == 0);
-            room.setQualityLevel(Room.QualityLevel.values()[(int)(Math.random() * 4)]);
             addRoomToDB(room);
         }
     }
