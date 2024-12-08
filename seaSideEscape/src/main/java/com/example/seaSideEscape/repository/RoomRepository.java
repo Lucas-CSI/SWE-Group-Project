03ab8ecb2f2c114a662ae7c1134b0b50a9842a26
Lucas-CSI <151797060+Lucas-CSI@users.noreply.github.com>
2024-12-07 23:06:33 -0600

    backend for getCart implemented

--- seaSideEscape/src/main/java/com/example/seaSideEscape/repository/RoomRepository.java
+++ seaSideEscape/src/main/java/com/example/seaSideEscape/repository/RoomRepository.java
@@ -1,9 +1,11 @@
 package com.example.seaSideEscape.repository;
 
 import com.example.seaSideEscape.model.Account;
+import com.example.seaSideEscape.model.Booking;
 import com.example.seaSideEscape.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -22,4 +24,7 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
     @Query("SELECT rm FROM Room rm WHERE rm NOT IN (SELECT rm2 FROM Room rm2 JOIN Booking b " +
             "ON b.room = rm2 JOIN Reservation res ON b.reservation = res WHERE res.checkInDate <= ?2 AND ?1 <= res.checkOutDate)")
     List<Room> getAvailableRooms(LocalDate checkInDate, LocalDate checkOutDate);
+
+    @Query("SELECT rm FROM Room rm JOIN Booking b ON rm = b.room WHERE :#{#account.unbookedReservation} = b.reservation")
+    List<Room> getCart(@Param("account") Account account);
 }
