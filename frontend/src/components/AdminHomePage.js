1ec9d6b4835ae6d54949b5c5aace6da2285d37f1
Lucas-CSI <151797060+Lucas-CSI@users.noreply.github.com>
2024-12-09 22:37:52 -0600

    created buttons and labels for modify permission and create clerk

--- frontend/src/components/AdminHomePage.js
+++ frontend/src/components/AdminHomePage.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
-import { Button, Box, Typography, TextField } from '@mui/material';
+import {Button, Box, Typography, TextField, Select, MenuItem, InputLabel, FormControl} from '@mui/material';
 
 const AdminHomepage = () => {
     const [email, setEmail] = useState('');
-
+    const [clerkEmail, setClerkEmail] = useState('');
+    const [clerkUsername, setClerkUsername] = useState('');
+    const [clerkInfo, setClerkInfo] = useState('');
+    const [modifyUsername, setModifyUsername] = useState('');
+    const [permissionLevel, setPermissionLevel] = useState(null);
+    const [modifyResult, setModifyResult] = useState('');
     const handleSearch = () => {
         console.log(`Searching for: ${email}`);
     };
@@ -102,7 +107,35 @@ const AdminHomepage = () => {
                         Check Out
                     </Button>
                 </Box>
-
+                <Box
+                    sx={{
+                        top: '100px',
+                        right: '20px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: '10px',
+                        backgroundColor: 'rgba(255, 255, 255, 0.9)',
+                        padding: '10px',
+                        borderRadius: '8px',
+                        boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+                    }}
+                >
+                    <TextField
+                        label="Username"
+                        variant="standard"
+                        size="small"
+                        value={clerkUsername}
+                        onChange={(e) => setClerkUsername(e.target.value)}
+                    />
+                    <TextField
+                        label="Email"
+                        variant="standard"
+                        size="small"
+                        value={clerkEmail}
+                        onChange={(e) => setClerkEmail(e.target.value)}
+                    />
+                    <p>{clerkInfo}</p>
+                </Box>
                 <Button
                     variant="contained"
                     sx={{
@@ -117,6 +150,56 @@ const AdminHomepage = () => {
                     }}
                 >
                     Create Clerk Account
+                </Button>
+
+                <Box
+                    sx={{
+                        top: '100px',
+                        right: '20px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: '10px',
+                        backgroundColor: 'rgba(255, 255, 255, 0.9)',
+                        padding: '10px',
+                        borderRadius: '8px',
+                        boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+                    }}
+                >
+                    <TextField
+                        label="Username"
+                        variant="standard"
+                        size="small"
+                        value={modifyUsername}
+                        onChange={(e) => setModifyUsername(e.target.value)}
+                    />
+                    <FormControl fullWidth>
+                        <InputLabel id="accountPermission">Permission</InputLabel>
+                        <Select
+                            onChange={(e) => setPermissionLevel(e.target.value)}
+                            labelId="accountPermission"
+                            label="Permission Level" variant="standard">
+                            <MenuItem value={0}>Guest</MenuItem>
+                            <MenuItem value={1}>Clerk</MenuItem>
+                            <MenuItem value={2}>Admin</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <p>{modifyResult}</p>
+                </Box>
+
+                <Button
+                    variant="contained"
+                    sx={{
+                        backgroundColor: 'rgb(25,122,140)',
+                        color: 'white',
+                        width: '100%',
+                        height: 60,
+                        '&:hover': {
+                            backgroundColor: '#28c1d8',
+                        },
+                        marginBottom: 4,
+                    }}
+                >
+                    Modify Account Permissions
                 </Button>
 
                 <Button
